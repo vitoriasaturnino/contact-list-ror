7b7ef33c23522c7773e5e0cda49a87e7f8ffbefd
Vitória Moura <vim.saturnino@gmail.com>
2022-08-09 19:00:04 -0300

    entendendo a comunicação entre view e controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+
+  # definindo a action view
+  def index 
+    # variavel de instancia 
+    @nome = "Vitória"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # controller#action
+  root to: "static_pages#index"
 end
